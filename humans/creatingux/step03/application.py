60634deb2a832e33999761e7b1cdf6c75dd13bb7
Levan <levan.list@gmail.com>
2018-06-17 12:26:24 +0200

    Update application.py

--- humans/creatingux/step03/application.py
+++ humans/creatingux/step03/application.py
@@ -5,6 +5,11 @@ from pyramid.config import Configurator
 def main():
     config = Configurator()
     config.scan("views")
+    # In newer versions of pyramid (>= 1.5) chameleon is not automatically included to the installation
+    # so to avoid "ValueError: No such renderer factory .pt" you need to
+    # 1) pip install pyramid_chameleon
+    # 2) add the following line
+    config.include('pyramid_chameleon') 
     app = config.make_wsgi_app()
     return app
 
